09f8d2f8fe308ca4e7c6a522e26a94b2f1511cb5
Igor Peonte <igor.144@gmail.com>
2017-04-05 13:26:38 -0400

    Moved event handler into authentication server

--- security-demo-auth-server/src/main/java/com/example/demo/rest/security/auth_server/AuthServerWebApp.java
+++ security-demo-auth-server/src/main/java/com/example/demo/rest/security/auth_server/AuthServerWebApp.java
@@ -2,6 +2,13 @@ package com.example.demo.rest.security.auth_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.example.rest.common.security.core.Constants;
 
 /**
  * Spring Boot Launcher
@@ -15,5 +22,19 @@ public class AuthServerWebApp {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServerWebApp.class, args);
 	}
+
+	/**
+	 * Inject user name was used during login into standard http session
+	 * 
+	 * @param event
+	 */
+	@EventListener
+	public void handleAuthenticationSuccessEvent(AuthenticationSuccessEvent event) {
+		(((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession(false))
+				.setAttribute(Constants.USER_NAME_KEY,
+						((UsernamePasswordAuthenticationToken) event.getSource()).getName());
+
+	}
+
 
 }
